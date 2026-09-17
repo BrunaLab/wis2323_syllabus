a13a284177f9481133a0ae01b077c2653cc24ffb
embruna <embruna@ufl.edu>
2026-09-17 11:30:09 -0400

    update ldg

--- class_sessions/07_patterns_biodiversity_latitudinal/ldg_plot.R
+++ class_sessions/07_patterns_biodiversity_latitudinal/ldg_plot.R
@@ -1,6 +1,7 @@
 library(tidyverse)
 library(ggrepel)
-data<-read_csv("./class_materials/class_sessions/07_patterns_biodiversity_latitudinal/ldg_data.csv")
+library(here)
+data<-read_csv(here("class_sessions","07_patterns_biodiversity_latitudinal","ldg_data.csv"))
 
 # load ggplot2
 
@@ -13,12 +14,12 @@ ggplot(data_mod, aes(x=lat, y=count, color=category, label=city)) +
   geom_point(size=2) +
   geom_vline(xintercept = 23,color="darkgray", linetype="dashed")+
   geom_vline(xintercept = -23,color="darkgray", linetype="dashed")+
-  facet_wrap(~category,ncol = 3) +
+  facet_wrap(~category,ncol = 3,scales="free") +
   stat_smooth(method = "lm",
               formula = y ~ poly(x, 2),
               se = FALSE)+
   coord_flip()+
-  geom_text(check_overlap = TRUE)
+  geom_text(check_overlap = TRUE)+
   geom_text_repel(
     data = data_mod,
     aes(
@@ -26,26 +27,26 @@ ggplot(data_mod, aes(x=lat, y=count, color=category, label=city)) +
       y = count,
       label = city
     ))
-
-
-ggplot(data_mod, aes(x=lat, y=count, color=category)) + 
-  geom_point(size=2) +
-  geom_vline(xintercept = 23,color="darkgray", linetype="dashed")+
-  geom_vline(xintercept = -23,color="darkgray", linetype="dashed")+
-  facet_wrap(~category,ncol = 1, scales = "free") +
-  stat_smooth(method = "lm",
-              formula = y ~ poly(x, 2),
-              se = FALSE)+
-  coord_flip()+
-  # geom_text(check_overlap = TRUE)
-  geom_text_repel(
-    data = data_mod,
-    aes(
-      x = lat,
-      y = count,
-      label = city
-    ))
-
+# 
+# 
+# ggplot(data_mod, aes(x=lat, y=count, color=category)) + 
+#   geom_point(size=2) +
+#   geom_vline(xintercept = 23,color="darkgray", linetype="dashed")+
+#   geom_vline(xintercept = -23,color="darkgray", linetype="dashed")+
+#   facet_wrap(~category,ncol = 1, scales = "free") +
+#   stat_smooth(method = "lm",
+#               formula = y ~ poly(x, 2),
+#               se = FALSE)+
+#   coord_flip()+
+#   # geom_text(check_overlap = TRUE)
+#   geom_text_repel(
+#     data = data_mod,
+#     aes(
+#       x = lat,
+#       y = count,
+#       label = city
+#     ))
+# 
 
 
 
